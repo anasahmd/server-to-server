942b3e8df822792a6c6d070482898ad974b24b2d
Anas Ahmad <anasahmad0239@gmail.com>
2026-05-30 13:54:48 +0530

    Remove unused imports

--- controller/userController.js
+++ controller/userController.js
@@ -4,10 +4,10 @@ import User from "../model/userModel.js";
 const userController = {};
 
 userController.getUserByUid = async (req, res) => {
-  const { id } = req.params;
+  const { uid } = req.params;
 
   try {
-    const userInDb = await User.findOne({ uid: id });
+    const userInDb = await User.findOne({ uid });
 
     if (userInDb) {
       return res.json({ message: 'Fetched from local', data: userInDb });
@@ -15,14 +15,14 @@ userController.getUserByUid = async (req, res) => {
     
     let userInRemote;
     try {
-      userInRemote = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+      userInRemote = await axios.get(`https://jsonplaceholder.typicode.com/users/${uid}`);
     } catch (err) {
       return res.status(404).json({ message: 'User not found either in local or remote' });
     }
     
     const { name, email, address: { city }} = userInRemote.data;
 
-    let user = new User({ name, email, city, uid: id });
+    let user = new User({ name, email, city, uid });
 
     user = await user.save();
     return res.json({ message: 'Fetched from remote', data: user });
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import configureDB from './config/db.js';
-import User from './model/userModel.js';
-import axios from 'axios';
 import userController from './controller/userController.js';
 dotenv.config();
 
@@ -11,7 +9,7 @@ const PORT = process.env.PORT;
 const app = express();
 configureDB();
 
-app.get('/api/users/:id', userController.getUserByUid);
+app.get('/api/users/:uid', userController.getUserByUid);
 
 app.listen(PORT, () => {
   console.log('Server started on PORT:', PORT);
